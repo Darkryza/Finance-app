b54229a1a5fe0656d8cee323b9c50250bd50dfc6
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-05-28 21:47:22 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-slate-950 text-white">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen w-full bg-slate-950 text-white">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
