b8cf41356b4dca8610077414c92bdc38319809b1
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-05-29 23:17:56 +0800

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,23 @@
+import { Calendar } from "lucide-react";
+
 const Dashboard = () => {
-  return <div className="flex">Dashboard</div>;
+  return (
+    <div>
+      <div className="flex px-8 py-10">
+        <div>
+          <div className="flex justify-center items-center">
+            <span className="text-4xl font-bold">Dashboard</span>
+            <span className="self-end ml-3 text-slate-400">Mei</span>
+          </div>
+          <div className="mt-3 text-slate-400">2026</div>
+        </div>
+        <div className="p-2 bg-gray-900 ml-5 rounded-xl text-slate-400 flex justify-center items-center">
+          <Calendar />
+          <div>Bulan ini</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
